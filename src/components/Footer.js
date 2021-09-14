1a3890b0c780cd7416defd92ae5d809fd4914235
princerori <4gossen@gmail.com>
2021-09-13 19:49:45 -0500

    adding icons to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import { RiTwitterLine } from "react-icons/ri";
+import { RiGithubLine } from "react-icons/ri";
 
 const footerStyle = {
     // choose bg and font colors from the navbar
-    backgroundColor: "black",
+    backgroundColor: "rgba(26, 26, 26, 0.7)",
     color: "white",
     position: "fixed",
     bottom: "0",
     width: "100%",
-    height: "50px",
+    height: "40px",
     textAlign: "center",
     display: "flex",
     justifyContent: "center",
@@ -16,32 +18,42 @@ const footerStyle = {
 };
 
 const gitHubStyle = {
-    backgroundColor: "white",
-    height: "10px",
-    width: "10px",
-    borderRadius: "50%",
+    color: "#b6b6b6",
+    height: "15px",
+    width: "15px",
+};
+
+const twitterStyle = {
+    color: "#b6b6b6",
+    height: "15px",
+    width: "15px",
+};
+
+const navLinkStyle = {
+    // height: "50px",
+    width: "20px",
+    textAlign: "center",
 };
 
 export default function Footer() {
     return (
         <div>
             <footer style={footerStyle}>
-                <Nav.Item>
-                    <Nav.Link
-                        href="https://github.com/Mid-Heavy"
-                        target="blank"
-                    >
-                        <div style={gitHubStyle}></div>
-                    </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link
-                        href="https://twitter.com/midheavy"
-                        target="blank"
-                    >
-                        Twitter
-                    </Nav.Link>
-                </Nav.Item>
+                <Nav.Link
+                    href="https://github.com/Mid-Heavy"
+                    target="blank"
+                    style={navLinkStyle}
+                >
+                    <RiGithubLine style={gitHubStyle} />
+                </Nav.Link>
+
+                <Nav.Link
+                    href="https://twitter.com/midheavy"
+                    target="blank"
+                    style={navLinkStyle}
+                >
+                    <RiTwitterLine style={twitterStyle} />
+                </Nav.Link>
             </footer>
         </div>
     );
