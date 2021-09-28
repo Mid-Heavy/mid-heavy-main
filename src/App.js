846a851ffdc16cd4ae99d19b63577478f939d765
Pandaphobic <c.steffes@live.ca>
2021-09-27 19:20:10 -0500

    404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css"
-import MHNavbar from "./components/Navbar"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Footer from "./components/Footer"
-import Contact from "./pages/Contact"
+import "./App.css";
+import MHNavbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -22,12 +23,15 @@ function App() {
           <Route exact path={"/contact"}>
             <Contact />
           </Route>
+          <Route exact path={""}>
+            <PageNotFound />
+          </Route>
         </Switch>
 
         <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
