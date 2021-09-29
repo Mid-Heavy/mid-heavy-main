8c45be1f47809929052477de5af58349bb4a9a7e
Chris Steffes <c.steffes@live.ca>
2021-09-29 17:37:54 -0500

    adjusted height and size of home page logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,21 @@
 import React from "react"
 import AnimLogo from "../components/AnimLogo"
-import { Container } from "react-bootstrap"
+import { Container, Jumbotron } from "react-bootstrap"
 
 const style = {
   position: "relative",
   zIndex: 2,
   textAlign: "center",
-  height: "100%",
-  width: "100%",
-  marginTop: "25%"
+  marginBottom: "3em"
 }
 
 export default function Home() {
   return (
-    <Container>
-      <Container style={style}>
-        <AnimLogo></AnimLogo>
+    <Container style={style}>
+      <Jumbotron style={{ backgroundColor: "rgba(0,0,0,0)", margin: "auto" }}>
+        <AnimLogo style={style} />
         <p>MidHeavy.tech</p>
-      </Container>
+      </Jumbotron>
     </Container>
   )
 }
