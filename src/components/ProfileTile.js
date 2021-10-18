c6fab3c40a6b008fac8d11f11c347583e0624ae2
princerori <4gossen@gmail.com>
2021-10-18 17:56:15 -0500

    tiles have left and right style based on props

--- src/components/ProfileTile.js
+++ src/components/ProfileTile.js
@@ -2,6 +2,91 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import Background1 from "../assets/moodyTree.png";
 
+// CSS THAT IS FOR BOTH
+
+// LEFT CSS
+const tileStyleLeft = {
+    fontFamily: "Rajdhani",
+    color: "white",
+    marginTop: "50px",
+    maxWidth: "700px",
+    zIndex: "1",
+    height: "300px",
+    marginRight: "auto",
+    marginLeft: "auto",
+    padding: "0",
+    backgroundImage: "url(" + Background1 + ")",
+    backgroundSize: "cover",
+};
+
+const tileShadowLeft = {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(26, 26, 26, 0.7)",
+    clipPath: "polygon(40% 0, 100% 0, 100% 100%, 20% 100%)",
+    zIndex: "2",
+};
+
+const bodyStyleLeft = {
+    margin: "0",
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr 1fr 1fr",
+    gridTemplateRows: "1fr 1fr 1fr auto 10px",
+    zIndex: "3",
+};
+
+const tileLoginStyleLeft = {
+    fontSize: "2rem",
+    fontWeight: "bold",
+    gridColumnStart: "1",
+    gridColumnEnd: "span 1",
+    gridRowStart: "3",
+    gridRowEnd: "span 1",
+    textAlign: "center",
+    alignSelf: "start",
+};
+
+const tileNameStyleLeft = {
+    fontSize: "2rem",
+    fontWeight: "bold",
+    gridColumnStart: "3",
+    gridColumnEnd: "span 2",
+    gridRowStart: "1",
+    gridRowEnd: "span 1",
+    textAlign: "right",
+};
+
+const tileBioStyleLeft = {
+    gridColumnStart: "2",
+    gridColumnEnd: "span 3",
+    gridRowStart: "2",
+    gridRowEnd: "span 3",
+    textAlign: "right",
+};
+
+const tileLogosStyleLeft = {
+    display: "flex",
+    flex: "inline",
+    justifyContent: "right",
+    gridColumnStart: "4",
+    gridColumnEnd: "span 1",
+    gridRowStart: "5",
+    gridRowEnd: "span 1",
+};
+
+const tileProfilePicLeft = {
+    borderRadius: "50%",
+    height: "100px",
+    width: "100px",
+    gridColumnStart: "1",
+    gridColumnEnd: "span 1",
+    gridRowStart: "1",
+    gridRowEnd: "span 2",
+    margin: "auto",
+};
+
+// RIGHT CSS
 const tileStyle = {
     fontFamily: "Rajdhani",
     color: "white",
@@ -21,7 +106,7 @@ const tileShadow = {
     width: "100%",
     height: "100%",
     backgroundColor: "rgba(26, 26, 26, 0.7)",
-    clipPath: "polygon(40% 0, 100% 0, 100% 100%, 20% 100%)",
+    clipPath: "polygon(0 0, 80% 0, 60% 100%, 0 100%)",
     zIndex: "2",
 };
 
@@ -33,10 +118,10 @@ const bodyStyle = {
     zIndex: "3",
 };
 
-const tileTitleLoginStyle = {
+const tileLoginStyle = {
     fontSize: "2rem",
     fontWeight: "bold",
-    gridColumnStart: "1",
+    gridColumnStart: "4",
     gridColumnEnd: "span 1",
     gridRowStart: "3",
     gridRowEnd: "span 1",
@@ -44,32 +129,43 @@ const tileTitleLoginStyle = {
     alignSelf: "start",
 };
 
-const tileTitleNameStyle = {
+const tileNameStyle = {
     fontSize: "2rem",
     fontWeight: "bold",
-    gridColumnStart: "3",
+    gridColumnStart: "1",
     gridColumnEnd: "span 2",
     gridRowStart: "1",
     gridRowEnd: "span 1",
-    textAlign: "right",
+    textAlign: "left",
 };
 
 const tileBioStyle = {
-    gridColumnStart: "2",
+    gridColumnStart: "1",
     gridColumnEnd: "span 3",
     gridRowStart: "2",
     gridRowEnd: "span 3",
-    textAlign: "right",
+    textAlign: "left",
 };
 
-const logos = {
+const tileLogosStyle = {
     display: "flex",
     flex: "inline",
-    justifyContent: "right",
-    gridColumnStart: "4",
+    justifyContent: "left",
+    gridColumnStart: "1",
     gridColumnEnd: "span 1",
     gridRowStart: "5",
     gridRowEnd: "span 1",
+};
+
+const tileProfilePic = {
+    borderRadius: "50%",
+    height: "100px",
+    width: "100px",
+    gridColumnStart: "4",
+    gridColumnEnd: "span 1",
+    gridRowStart: "1",
+    gridRowEnd: "span 2",
+    margin: "auto",
 };
 
 /// TEMPLATE DATA OBJECT
@@ -114,34 +210,80 @@ const TEMP_PROFILE_DATA = {
 export default function ProfileTile(props) {
     const groupData = props.data;
     console.log(groupData);
+
+    const isLeft = props.isLeft;
+    if (isLeft) {
+        return (
+            <div>
+                <Card style={tileStyleLeft}>
+                    <div style={tileShadowLeft}></div>
+                    <Card.Body style={bodyStyleLeft}>
+                        <Card.Title style={tileLoginStyleLeft}>
+                            {TEMP_PROFILE_DATA.login}
+                        </Card.Title>
+                        <Card.Title style={tileNameStyleLeft}>
+                            {TEMP_PROFILE_DATA.name}
+                        </Card.Title>
+                        <img
+                            style={tileProfilePicLeft}
+                            src={TEMP_PROFILE_DATA.avatar_url}
+                        ></img>
+                        <Card.Text style={tileBioStyleLeft}>
+                            {TEMP_PROFILE_DATA.bio}
+                        </Card.Text>
+                        <div style={tileLogosStyleLeft}>
+                            <div
+                                style={{
+                                    height: "10px",
+                                    width: "10px",
+                                    backgroundColor: "white",
+                                    borderRadius: "50%",
+                                    marginRight: "2px",
+                                }}
+                            ></div>
+                            <div
+                                style={{
+                                    height: "10px",
+                                    width: "10px",
+                                    backgroundColor: "white",
+                                    borderRadius: "50%",
+                                    marginRight: "2px",
+                                }}
+                            ></div>
+                            <div
+                                style={{
+                                    height: "10px",
+                                    width: "10px",
+                                    backgroundColor: "white",
+                                    borderRadius: "50%",
+                                    marginRight: "2px",
+                                }}
+                            ></div>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
     return (
         <div>
             <Card style={tileStyle}>
                 <div style={tileShadow}></div>
                 <Card.Body style={bodyStyle}>
-                    <Card.Title style={tileTitleLoginStyle}>
+                    <Card.Title style={tileLoginStyle}>
                         {TEMP_PROFILE_DATA.login}
                     </Card.Title>
-                    <Card.Title style={tileTitleNameStyle}>
+                    <Card.Title style={tileNameStyle}>
                         {TEMP_PROFILE_DATA.name}
                     </Card.Title>
                     <img
-                        style={{
-                            borderRadius: "50%",
-                            height: "100px",
-                            width: "100px",
-                            gridColumnStart: "1",
-                            gridColumnEnd: "span 1",
-                            gridRowStart: "1",
-                            gridRowEnd: "span 2",
-                            margin: "auto",
-                        }}
+                        style={tileProfilePic}
                         src={TEMP_PROFILE_DATA.avatar_url}
                     ></img>
                     <Card.Text style={tileBioStyle}>
                         {TEMP_PROFILE_DATA.bio}
                     </Card.Text>
-                    <div style={logos}>
+                    <div style={tileLogosStyle}>
                         <div
                             style={{
                                 height: "10px",
